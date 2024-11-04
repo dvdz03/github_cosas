450fd822465d496c3978471ba5907ca6999e73c3
dvdz03 <dflores04@alumnos.uaq.mx>
2024-11-04 15:16:42 -0600

    nada más para ver si si lo hice bien

--- scripts/cosasdehoy.R
+++ scripts/cosasdehoy.R
@@ -1,3 +1,5 @@
 #secuencias,alineamiento 2 métodos, gráficos alineamiento y árbol
 cosasdehoy<-c("secuencia","alinea","dos met","Grafic","arbol")
 cosasdehoy
+nuevacosa<-sample(1:1234567890,16)
+nuevacosa
